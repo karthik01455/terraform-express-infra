9878c3e2cd0e7d6b889932f7587aeb389326512d
apoorvGit <apoorvmishra1000@gmail.com>
2023-04-19 01:39:02 +0530

    initialisation

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+
+// Endpoint to add a color to the database
+app.use(express.json());
+app.post('/colors', async (req, res) => {
+  try {
+    console.log(req.body)
+    const { name } = req.body;
+    const color = await prisma.color.create({
+      data: {
+        name,
+      },
+    });
+    res.json(color);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint to fetch all colors from the database
+app.get('/colors', async (req, res) => {
+  try {
+    const colors = await prisma.color.findMany();
+    res.json(colors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
---
+++ migrations/20230418195732_yes/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Color" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Color_pkey" PRIMARY KEY ("id")
+);
